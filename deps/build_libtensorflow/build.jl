d6cd23c5ac4d3277c761ac1155775679a12abe2c
Jon Malmaud <malmaud@gmail.com>
2017-03-24 13:03:21 -0400

    Build tweaks

--- deps/build_libtensorflow/build.jl
+++ deps/build_libtensorflow/build.jl
@@ -2,10 +2,13 @@ prefix = joinpath(ENV["HOME"], "tmp", "out")
 gs_bucket = "malmaud-stuff"
 version = "1.1.0"
 
-run(`docker run -v /var/run/docker.sock:/var/run/docker.sock -e "prefix=$prefix" buildtf`)
+#run(`docker run -v /var/run/docker.sock:/var/run/docker.sock -e "prefix=$prefix" buildtf`)
+
+full_names = []
 
 function upload(dir, name)
-    full_name = "tensorflow_$name_$version.zip"
+    full_name = "tensorflow_$(name)_$(version).zip"
+    push!(full_names, full_name)
     cd(joinpath(prefix, dir))
     run(`zip $full_name libtensorflow.so`)
     run(`gsutil cp $full_name gs://$gs_bucket`)
@@ -19,4 +22,6 @@ upload("mac", "mac")
 
 cd(start_dir)
 
-run(`gsutil acl -m ch -u AllUsers:R gs://$gs_bucket/*`)
+for name in full_names
+    run(`gsutil acl ch -u AllUsers:R gs://$gs_bucket/$name`)
+end
